2ae0e56abb02b52ec1ceaeb2d32543f066d2672d
liyunlong <liyunlong8859@gmail.com>
2017-02-10 16:18:38 +0800

    Support  Carthage

---
+++ GDPerformanceViewFramework/GDPerformanceViewFramework.h
@@ -0,0 +1,22 @@
+//
+//  GDPerformanceViewFramework.h
+//  GDPerformanceViewFramework
+//
+//  Created by Lee on 2017/2/10.
+//  Copyright © 2017年 Gavrilov Daniil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GDPerformanceViewFramework.
+FOUNDATION_EXPORT double GDPerformanceViewFrameworkVersionNumber;
+
+//! Project version string for GDPerformanceViewFramework.
+FOUNDATION_EXPORT const unsigned char GDPerformanceViewFrameworkVersionString[];
+
+#import <GDPerformanceViewFramework/GDPerformanceMonitor.h>
+#import <GDPerformanceViewFramework/GDPerformanceView.h>
+#import <GDPerformanceViewFramework/GDMarginLabel.h>
+#import <GDPerformanceViewFramework/GDWindowViewController.h>
+#import <GDPerformanceViewFramework/GDPerformanceMonitorProtocol.h>
+
