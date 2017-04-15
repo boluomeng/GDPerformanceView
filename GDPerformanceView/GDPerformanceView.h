cfab0e38ed7f3f26cf95b7cb539f0a3113163022
liyunlong <liyunlong8859@gmail.com>
2017-04-15 17:57:06 +0800

    update

---
+++ GDPerformanceView/GDPerformanceView.h
@@ -0,0 +1,20 @@
+//
+//  GDPerformanceView.h
+//  GDPerformanceView
+//
+//  Created by 李云龙 on 2017/4/15.
+//  Copyright © 2017年 Gavrilov Daniil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GDPerformanceView.
+FOUNDATION_EXPORT double GDPerformanceViewVersionNumber;
+
+//! Project version string for GDPerformanceView.
+FOUNDATION_EXPORT const unsigned char GDPerformanceViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GDPerformanceView/PublicHeader.h>
+#import <GDPerformanceView/GDPerformanceView.h>
+#import <GDPerformanceView/GDPerformanceMonitorProtocol.h>
+
