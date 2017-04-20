3c1c64f6dfbe436c5d131bf7f7abebaa689ff470
liyunlong <liyunlong8859@gmail.com>
2017-04-20 09:44:26 +0800

    update

---
+++ GDPerformanceViewFramework/GDPerformanceViewFramework.h
@@ -0,0 +1,23 @@
+//
+//  GDPerformanceViewFramework.h
+//  GDPerformanceViewFramework
+//
+//  Created by 李云龙 on 2017/4/20.
+//  Copyright © 2017年 Gavrilov Daniil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GDPerformanceViewFramework.
+FOUNDATION_EXPORT double GDPerformanceViewFrameworkVersionNumber;
+
+//! Project version string for GDPerformanceViewFramework.
+FOUNDATION_EXPORT const unsigned char GDPerformanceViewFrameworkVersionString[];
+
+#import <GDPerformanceViewFramework/GDPerformanceMonitor.h>
+#import <GDPerformanceViewFramework/GDPerformanceView.h>
+#import <GDPerformanceViewFramework/GDMarginLabel.h>
+#import <GDPerformanceViewFramework/GDWindowViewController.h>
+#import <GDPerformanceViewFramework/GDPerformanceMonitorProtocol.h>
+
+
